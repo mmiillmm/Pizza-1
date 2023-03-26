b994c9f256d73e2e35b7d8fb12dbf8a85976b28a
Marci02 <nagymarci.0218@gmail.com>
2023-03-26 19:37:14 +0200

    Csak @media query kell

--- 20230313  Pizzéria cssgrid oldal készítése/script.js
+++ 20230313  Pizzéria cssgrid oldal készítése/script.js
@@ -1,12 +1,42 @@
-function szamolas(){
-    let pizzanev = pizza.value
-    console.log(pizzanev)
-    switch (pizzanev) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
-    }
+function szamolas() {
+    // Kiválasztott pizza típusok és mennyiségek elérése
+    const nevek = document.querySelectorAll(".nevek");
+    const pizza1 = nevek[0].value;
+    const pizza2 = nevek[1].value;
+    const mennyisegek = document.querySelectorAll("#mennyiseg select");
+    const mennyiseg1 = mennyisegek[0].value;
+    const mennyiseg2 = mennyisegek[1].value;
+  
+    // Pizza árak
+    const arak = {
+      Margherita: 1800,
+      Magyaros: 2070,
+      Mexikói: 2080,
+      "Sokk-sajtos": 2100,
+      "Son-kukori": 1930,
+      Sonkás: 1880,
+    };
+  
+    // Sorok árának kiszámítása
+    const osszeg1 = arak[pizza1] * mennyiseg1;
+    const osszeg2 = arak[pizza2] * mennyiseg2;
+  
+    //Sorok árának megjelenítése
+    const osszegInputok = document.querySelectorAll("#Ar #osszeg1");
+    osszegInputok.forEach((input) => {
+      input.value = osszeg1;
+    });
+
+    const osszegInputok2 = document.querySelectorAll("#Ar #osszeg2");
+    osszegInputok2.forEach((input) => {
+      input.value = osszeg2;
+    });
+
+
+    //Összes kiszámolása
+    const vege = arak[pizza1] * mennyiseg1 + arak[pizza2] * mennyiseg2
+
+
+    //Összes megjelenítése
+    document.getElementById("ar").innerHTML = vege + ".-";
 }
